a88f343943948c64bc55ac4a180e3d785db68df4
Melissa <melissa.j.bica@gmail.com>
2014-11-04 15:19:07 -0700

    tweets on clicking go

--- static/js/display-tweets.js
+++ static/js/display-tweets.js
@@ -1,23 +1,30 @@
 $(document).ready(function() {
   var secondsBetweenReloads = 5;
-  var endpoint = '/data';
 
-  /*
-   * load tweets from our rest endpoint and list the in the tweet-listing <ul>
-   */
-  var loadTweets = function(number, topic) {
-    var query = '?number=' + number + '&' + 'topic=' + topic;
-    $.ajax({
-      url: endpoint + query
-    }).then(function(data) {
-      data = $.parseJSON(data)
-      $('#tweet-listing').empty();
-      for (var i = 0; i < data.length; i++) {
-        $('#tweet-listing').append($('<li/>').append(data[i]));
-      }
-    });
-  };
-
-  loadTweets(10);
-  window.setInterval(function() {loadTweets(10);}, secondsBetweenReloads * 1000);
+  // loadTweets(10);
+  // window.setInterval(function() {loadTweets(10);}, secondsBetweenReloads * 1000);
 });
+
+/*
+ * load tweets from our rest endpoint and list the in the tweet-listing <ul>
+ */
+function loadTweets(number, topic) {
+  var query = '/data?number=' + number + '&' + 'topic=' + topic;
+  console.log(query);
+  $.ajax({
+    url: query
+  }).then(function(data) {
+    data = $.parseJSON(data);
+    $('#tweet-listing').empty();
+    for (var i = 0; i < data.length; i++) {
+      $('#tweet-listing').append($('<li/>').append(data[i]));
+    }
+  });
+};
+
+function getTweets(){
+  console.log('here');
+  var topic = $('#search-box').val();
+  loadTweets(10);
+  return false;
+}
--- tweets.py
+++ tweets.py
@@ -1,2 +1,2 @@
 def get_tweets():
-    return ['example tweet1', 'example tweet2']
+    return ['example tweet1', 'example tweet2', 'example tweet2', 'example tweet2', 'example tweet2', 'example tweet2', 'example tweet2', 'example tweet2', 'example tweet2', 'example tweet2', 'example tweet2']
