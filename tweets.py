ef3fd8870f07e80255c1237b9e368212d52ef7f5
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-11-06 13:05:48 -0700

    add new process for handling tweet stream

--- tweets.py
+++ tweets.py
@@ -1,2 +1,10 @@
-def get_tweets():
-    return ['example tweet1', 'example tweet2', 'example tweet2', 'example tweet2', 'example tweet2', 'example tweet2', 'example tweet2', 'example tweet2', 'example tweet2', 'example tweet2', 'example tweet2']
+import time
+
+def get_tweets(pipe):
+    """This method defines a process that gets tweets from the Twitter streaming API."""
+    num = 0
+    while (True):
+        tweets = ['example tweet {}'.format(str(i)) for i in range(num, num + 10)]
+        pipe.send(tweets)
+        num += 10
+        time.sleep(5)
