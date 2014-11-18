8d2024e37f7f2c4177119007557c85ef4ef59c88
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-11-18 15:53:31 -0700

    add trending.js

---
+++ static/js/trending.js
@@ -0,0 +1,58 @@
+
+// Load the Visualization API and the piechart package.
+google.load('visualization', '1.0', {'packages':['corechart']});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.setOnLoadCallback(loadHashtagData);
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart(hashtags) {
+
+  // Create the data table.
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Hashtag');
+  data.addColumn('number', 'Number of Tweets');
+
+  hashtags = [];
+  if (hashtags.length === 0) {
+    hashtags = [
+      {tag: 'Mushrooms', count: 3},
+      {tag: 'Onions', count: 1},
+      {tag: 'Olives', count: 1},
+      {tag: 'Zucchini', count: 1},
+      {tag: 'Pepperoni', count: 2},
+      {tag: 'Misc', count: 0}
+    ];
+  }
+
+  hashtags = hashtags.sort(function (a, b) {
+    return b.count - a.count;
+  });
+
+  for (var i = 0; i < 5; i++) {
+    if (i > hashtags.length) {
+      break;
+    }
+    data.addRow([hashtags[i].tag, hashtags[i].count]);
+  }
+
+  var options = {};
+  var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
+
+function loadHashtagData() {
+  var query = '/hashtagmap';
+  $('#chart-div').empty();
+  $('#spinner').empty()
+  $('#spinner').append('<img src="/static/img/ajax-loader.gif">')
+  $.ajax({
+    url: query
+  }).then(function(data) {
+    $('#spinner').empty()
+    data = $.parseJSON(data);
+    drawChart(data);
+  });
+};
